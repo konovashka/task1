34bbf6b548adcbbf202b816678456cc17ff6f993
konovashka <anton_konovalov_1@mail.ru>
2021-11-08 23:19:15 +0300

    Добавьте файлы проекта.

---
+++ task1/task1.cpp
@@ -0,0 +1,21 @@
+﻿//определение суммы и количества цифр числа
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, sum, count, ost;
+	sum = 0;
+	ost = 0;
+	count = 0;
+	scanf_s("%d", &n);
+	while (n > 0)
+	{
+		ost = n % 10;
+		sum += ost;
+		count += 1;
+		n = n / 10;
+	}
+	printf("%d", sum);
+	printf("%d", count);
+	return 0;
+}
